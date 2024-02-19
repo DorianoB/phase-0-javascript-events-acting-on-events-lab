e0403ba0ce9b6f9f3229498b9d0f1b7c46d52568
Doriano <doriano.bergamin18@gmail.com>
2024-02-19 16:34:26 +1100

    events acting

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", ""); // Change to "right"
+  const right = parseInt(rightNumbers, 10);
+
+  // You should increment the right value, not decrement
+  dodger.style.left = `${right + 1}px`;
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
